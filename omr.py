196fec2cbe40c89b6043da0013521fdcee6ce4f9
ElTinMar <martin.privat@hotmail.fr>
2023-10-20 22:00:15 +0200

    figured out how to make a grating

--- omr.py
+++ omr.py
@@ -6,35 +6,49 @@ import numpy as np
 from multiprocessing import Process, Queue
 import time
 
-VERT_SHADER = """
-attribute vec2 a_position;
-uniform float u_size;
 
-void main() {
-    gl_Position = vec4(a_position, 0.0, 1.0);
-    gl_PointSize = u_size;
-}
+
+VERT_SHADER = """
+attribute vec2 position;
+attribute float phase;
+varying float v_phase;
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+    v_phase = phase;
+} 
 """
 
+# Fragment Shaders have the following built-in input variables. 
+# in vec4 gl_FragCoord;
+# in bool gl_FrontFacing;
+# in vec2 gl_PointCoord;
+
 FRAG_SHADER = """
-void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-}
+varying float v_phase;
+void main()
+{
+    const float tau = 2.0*3.14159;
+    const float freq = 0.01;
+    float value = 0.5 + 0.5 * sin(freq*tau*gl_FragCoord.x + v_phase);
+    gl_FragColor = vec4(value, value, value, 1.0);
+} 
 """
 
 
 class Canvas(app.Canvas):
-    def __init__(self, queue):
-        app.Canvas.__init__(self, keys='interactive')
+    def __init__(self):
+        app.Canvas.__init__(self, size=(1024,1024), keys='interactive')
 
-        self.queue = queue
         ps = self.pixel_scale
-        
+        self.phase = 0
 
         self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
-        self.pos = np.array([[0,0]], dtype=np.float32)
-        self.program['a_position'] = self.pos
-        self.program['u_size'] = 20.*ps
+
+        # Set uniforms and attributes
+        self.program['phase'] = 0
+        self.program['position'] = [(-1, -1), (-1, +1),
+                                    (+1, -1), (+1, +1)]
  
         self.timer = app.Timer('auto', self.on_timer)
         self.timer.start()
@@ -47,55 +61,17 @@ class Canvas(app.Canvas):
 
     def on_draw(self, event):
         gloo.clear('black')
-        self.program.draw('points')
+        self.program.draw('triangle_strip')
 
+    
     def on_timer(self, event):
-        self.pos = self.queue.get()
-        self.program['a_position'] = self.pos
+        self.phase += 20 * 1/60 
+        self.program['phase'] = self.phase
         self.update()
-        
-    def on_key_press(self, event):
-        h_inc = np.array([[0.01, 0.0]], dtype=np.float32)
-        v_inc = np.array([[0.0, 0.01]], dtype=np.float32)
-        
-        if event.text == 'a':
-            self.pos -= h_inc 
-        if event.text == 'd':
-            self.pos += h_inc 
-        if event.text == 'w':
-            self.pos += v_inc 
-        if event.text == 's':
-            self.pos -= v_inc 
-            
-        self.program['a_position'] = self.pos
-        
-        self.update()
-
-def monitor(queue):
-    while True:
-        print(queue.qsize())    
-        time.sleep(0.1)
-            
-def producer(queue):
-    pos = np.array([[0.0,0.0]], dtype=np.float32)
-    alpha_inc = 0.1
-    alpha = 0
-    while True:
-        alpha = alpha + alpha_inc
-        pos[0,0] = 0.5*np.cos(alpha)
-        pos[0,1] = 0.5*np.sin(alpha)
-        queue.put(pos)
-        time.sleep(0.017)
     
     
 if __name__ == '__main__':
-    q = Queue()
-    prod = Process(target=producer, args=(q,))
-    mon = Process(target=monitor, args=(q,))
-    prod.start()
-    mon.start()
-    canvas = Canvas(q)
+    canvas = Canvas()
     if sys.flags.interactive != 1:
         app.run()
-    prod.terminate()
-    mon.terminate()
+    
