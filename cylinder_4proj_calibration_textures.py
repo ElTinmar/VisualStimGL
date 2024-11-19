da63e085dc933c9a856bf76377ed004caa643b7e
ElTinmar <martin.privat@hotmail.fr>
2024-11-19 13:36:07 +0100

    update

--- cylinder_4proj_calibration_textures.py
+++ cylinder_4proj_calibration_textures.py
@@ -34,6 +34,9 @@ def checkerboard(grid_num=8, grid_size=32):
     Z = np.row_stack(grid_num // 2 * (row_even, row_odd)).astype(np.uint8)
     return 255 * Z.repeat(grid_size, axis=0).repeat(grid_size, axis=1)
 
+def target():
+    pass
+
 use(gl='gl+')
 
 VERT_SHADER_CYLINDER ="""
