8162b9c40c66e9a6dbbf6ed692c6bd48b9d8a236
ElTinmar <martin.privat@hotmail.fr>
2024-11-26 18:31:54 +0100

    structured

--- solve_cyl_equation.py
+++ solve_cyl_equation.py
@@ -1,4 +1,7 @@
 from sympy import solve, symbols, init_printing
+import numpy as np
+
+# solving
 
 init_printing()
 
@@ -10,8 +13,8 @@ x_f, y_f, z_f = symbols('x_f, y_f, z_f', constant=True)
 # intersect straight line between fish (x,y,z) and each vertex with cylinder.
 # need two equations for the straight line, and one for the cylinder
 equations = [
-    (x - x_f)/(x_v -x_f) - (y - y_f)/(y_v - y_f), # on straight line between vertex and fish
-    (x - x_f)/(x_v -x_f) - (z - z_f)/(z_v - z_f), # on straight line between vertex and fish
+    (x - x_f)/(x_v - x_f) - (y - y_f)/(y_v - y_f), # on straight line between vertex and fish
+    (x - x_f)/(x_v - x_f) - (z - z_f)/(z_v - z_f), # on straight line between vertex and fish
     x**2 +  z**2 - r**2 # on cylinder of radius r
 ]
 solutions = solve(equations, [x,y,z], dict=True)
@@ -19,145 +22,138 @@ solutions = solve(equations, [x,y,z], dict=True)
 solutions[0]
 solutions[1]
 
-import numpy as np
+# implementation
 
-x_v = -2
-y_v = 0
-z_v = -2
+def solutions0(v, f, r):
 
-x_f = 0
-y_f = 0
-z_f = -5
+    # vertex coords
+    x_v = v[0]
+    y_v = v[1]
+    z_v = v[2]
 
-r = 100
+    # fish coords
+    x_f = f[0]
+    y_f = f[1]
+    z_f = f[2]
 
-# (x_f-x_v)**2 + (z_f-z_v)**2
-denominator = (
-    + x_f*x_f 
-    - 2*x_f*x_v 
-    + x_v*x_v 
-    + z_f*z_f 
-    - 2*z_f*z_v 
-    + z_v*z_v
-)
+    # (x_f-x_v)**2 + (z_f-z_v)**2
+    denominator = (
+        + x_f*x_f 
+        - 2*x_f*x_v 
+        + x_v*x_v 
+        + z_f*z_f 
+        - 2*z_f*z_v 
+        + z_v*z_v
+    )
 
-# r**2 * [ (x_f-x_v)**2 + (z_f-z_v)**2 ] - (x_f*z_v - x_v*z_f)**2 
-squareroot = np.sqrt(
-    + r*r*x_f*x_f 
-    - 2*r*r*x_f*x_v 
-    + r*r*x_v*x_v 
-    + r*r*z_f*z_f 
-    - 2*r*r*z_f*z_v 
-    + r*r*z_v*z_v 
-    - x_f*x_f*z_v*z_v 
-    + 2*x_f*x_v*z_f*z_v 
-    - x_v*x_v*z_f*z_f
-)
+    # r**2 * [ (x_f-x_v)**2 + (z_f-z_v)**2 ] - (x_f*z_v - x_v*z_f)**2 
+    squareroot = np.sqrt(
+        + r*r*x_f*x_f 
+        - 2*r*r*x_f*x_v 
+        + r*r*x_v*x_v 
+        + r*r*z_f*z_f 
+        - 2*r*r*z_f*z_v 
+        + r*r*z_v*z_v 
+        - x_f*x_f*z_v*z_v 
+        + 2*x_f*x_v*z_f*z_v 
+        - x_v*x_v*z_f*z_f
+    )
 
-# x_v * [z_f*(z_f-z_v) + squareroot] - x_f * [z_v*(z_f-z_v) + squareroot]
-x0 = 1/denominator * (
-    - x_f*z_f*z_v 
-    + x_f*z_v*z_v 
-    - x_f*squareroot 
-    + x_v*z_f*z_f 
-    - x_v*z_f*z_v 
-    + x_v*squareroot
-)
+    # x_v * [z_f*(z_f-z_v) + squareroot] - x_f * [z_v*(z_f-z_v) + squareroot]
+    x0 = 1/denominator * (
+        - x_f*z_f*z_v 
+        + x_f*z_v*z_v 
+        - x_f*squareroot 
+        + x_v*z_f*z_f 
+        - x_v*z_f*z_v 
+        + x_v*squareroot
+    )
 
-# x_v * [z_f*(z_f-z_v) - squareroot] - x_f * [z_v*(z_f-z_v) - squareroot]
-x1 = 1/denominator * (
-    - x_f*z_f*z_v 
-    + x_f*z_v*z_v 
-    + x_f*squareroot 
-    + x_v*z_f*z_f 
-    - x_v*z_f*z_v 
-    - x_v*squareroot
-)
+    # x_v * [z_f*(z_f-z_v) - squareroot] - x_f * [z_v*(z_f-z_v) - squareroot]
+    x1 = 1/denominator * (
+        - x_f*z_f*z_v 
+        + x_f*z_v*z_v 
+        + x_f*squareroot 
+        + x_v*z_f*z_f 
+        - x_v*z_f*z_v 
+        - x_v*squareroot
+    )
 
-# (x_f - x_v)*(x_f*y_v - x_v*y_f) + y_v * [z_f*(z_f-z_v) + squareroot] - y_f * [z_v*(z_f-z_v) + squareroot]
-y0 = 1/denominator * (
-    + x_f*x_f*y_v 
-    - x_f*x_v*y_f 
-    - x_f*x_v*y_v 
-    + x_v*x_v*y_f 
+    # (x_f - x_v)*(x_f*y_v - x_v*y_f) + y_v * [z_f*(z_f-z_v) + squareroot] - y_f * [z_v*(z_f-z_v) + squareroot]
+    y0 = 1/denominator * (
+        + x_f*x_f*y_v 
+        - x_f*x_v*y_f 
+        - x_f*x_v*y_v 
+        + x_v*x_v*y_f 
 
-    - y_f*z_f*z_v 
-    + y_f*z_v*z_v 
-    - y_f*squareroot 
+        - y_f*z_f*z_v 
+        + y_f*z_v*z_v 
+        - y_f*squareroot 
 
-    + y_v*z_f*z_f 
-    - y_v*z_f*z_v 
-    + y_v*squareroot
-)
+        + y_v*z_f*z_f 
+        - y_v*z_f*z_v 
+        + y_v*squareroot
+    )
 
-# (x_f - x_v)*(x_f*y_v - x_v*y_f) + y_v * [z_f*(z_f-z_v) - squareroot] - y_f * [z_v*(z_f-z_v) - squareroot]
-y1 = 1/denominator * (
-    + x_f*x_f*y_v 
-    - x_f*x_v*y_f 
-    - x_f*x_v*y_v 
-    + x_v*x_v*y_f 
+    # (x_f - x_v)*(x_f*y_v - x_v*y_f) + y_v * [z_f*(z_f-z_v) - squareroot] - y_f * [z_v*(z_f-z_v) - squareroot]
+    y1 = 1/denominator * (
+        + x_f*x_f*y_v 
+        - x_f*x_v*y_f 
+        - x_f*x_v*y_v 
+        + x_v*x_v*y_f 
 
-    - y_f*z_f*z_v 
-    + y_f*z_v*z_v 
-    + y_f*squareroot 
+        - y_f*z_f*z_v 
+        + y_f*z_v*z_v 
+        + y_f*squareroot 
 
-    + y_v*z_f*z_f 
-    - y_v*z_f*z_v 
-    - y_v*squareroot
-)
+        + y_v*z_f*z_f 
+        - y_v*z_f*z_v 
+        - y_v*squareroot
+    )
 
-z0 = 1/denominator * (
-    + (x_f - x_v)*(x_f*z_v - x_v*z_f) 
-    - (z_f - z_v) * squareroot
-)
-z1 = 1/denominator * (
-    + (x_f - x_v)*(x_f*z_v - x_v*z_f) 
-    + (z_f - z_v) * squareroot
-)
+    z0 = 1/denominator * (
+        + (x_f - x_v)*(x_f*z_v - x_v*z_f) 
+        - (z_f - z_v) * squareroot
+    )
+    z1 = 1/denominator * (
+        + (x_f - x_v)*(x_f*z_v - x_v*z_f) 
+        + (z_f - z_v) * squareroot
+    )
 
-sol0 = np.array([x0,y0,z0])
-sol1 = np.array([x1,y1,z1])
-fish = np.array([x_f,y_f,z_f])
-vertex = np.array([x_v,y_v,z_v])
+    sol0 = np.array([x0,y0,z0])
+    sol1 = np.array([x1,y1,z1])
+    return (sol0, sol1)
 
-np.dot(sol0-fish,vertex-fish)
-np.dot(sol1-fish,vertex-fish)
+def solutions1(v, f, r):
 
-# factorized
+    # vertex coords
+    x_v = v[0]
+    y_v = v[1]
+    z_v = v[2]
 
-import numpy as np
+    # fish coords
+    x_f = f[0]
+    y_f = f[1]
+    z_f = f[2]
 
-x_v = -2
-y_v = 0
-z_v = -2
+    # helpful variables;
+    x_ = x_f-x_v
+    z_ = z_f-z_v
+    xz_ = (x_f*z_v - x_v*z_f)
+    xy_ = (x_f*y_v - x_v*y_f)
+    d = x_*x_ + z_*z_
+    s = np.sqrt(r*r*d - xz_*xz_)
 
-x_f = 0
-y_f = 0
-z_f = -5
+    # projection to cylinder
+    x0 = 1/d * (x_v*(z_f*z_ + s) - x_f*(z_v*z_ + s))
+    x1 = 1/d * (x_v*(z_f*z_ - s) - x_f*(z_v*z_ - s))
+    y0 = 1/d * (x_*xy_ + y_v * (z_f*z_ + s) - y_f * (z_v*z_ + s))
+    y1 = 1/d * (x_*xy_ + y_v * (z_f*z_ - s) - y_f * (z_v*z_ - s))
+    z0 = 1/d * (x_*xz_ - z_*s)
+    z1 = 1/d * (x_*xz_ + z_*s)
 
-r = 100
+    sol0 = np.array([x0,y0,z0])
+    sol1 = np.array([x1,y1,z1])
+    return (sol0, sol1)
 
-# helpful variables;
-x_ = x_f-x_v
-y_ = y_f-y_v
-z_ = z_f-z_v
-xz_ = (x_f*z_v - x_v*z_f)
-xy_ = (x_f*y_v - x_v*y_f)
-d = x_*x_ + z_*z_
-s = np.sqrt(r*r*d - xz_*xz_)
-
-# projection to cylinder
-x0 = 1/d * (x_v*(z_f*z_ + s) - x_f*(z_v*z_ + s))
-x1 = 1/d * (x_v*(z_f*z_ - s) - x_f*(z_v*z_ - s))
-y0 = 1/d * (x_*xy_ + y_v * (z_f*z_ + s) - y_f * (z_v*z_ + s))
-y1 = 1/d * (x_*xy_ + y_v * (z_f*z_ - s) - y_f * (z_v*z_ - s))
-z0 = 1/d * (x_*xz_ - z_*s)
-z1 = 1/d * (x_*xz_ + z_*s)
-
-sol0 = np.array([x0,y0,z0])
-sol1 = np.array([x1,y1,z1])
-fish = np.array([x_f,y_f,z_f])
-vertex = np.array([x_v,y_v,z_v])
-
-np.dot(sol0-fish,vertex-fish)
-np.dot(sol1-fish,vertex-fish)
+solutions1([-2,0,-2],[0,0,-5],100)
